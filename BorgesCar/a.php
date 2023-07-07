d6101f1683bf2c8f80ecab240675cbd3ba84f0c6
riquelmefINFO <133125961+riquelmefINFO@users.noreply.github.com>
2023-07-07 15:43:09 -0300

    Delete a.php

--- BorgesCar/a.php
+++
@@ -1,4 +0,0 @@
-" . btn_editar_excluir(
-    'cliente.php?codigo=' . $cliente['cli_codigo'] . '&nome=' . $cliente['cli_nome'] . '&email=' . $cliente['cli_email'] . '&senha=' . $cliente['cli_senha'] . '&cpf=' . $cliente['cli_cpf'] . '&rg=' . $cliente['cli_rg'] . '&endereco=' . $cliente['cli_endereco'] . '',
-    '../Controller/rotas.php?acao=excluir&tipo=cliente&codigo=' . $cliente['cli_codigo'] . ''
-) . "
